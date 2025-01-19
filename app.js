064d07a6b8c4bc1eb18053be1c60294c10c24b86
SScliff <sdanielsousa.024@gmail.com>
2025-01-19 15:05:27 -0300

    Implementado a verificação de caracteres especiais

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ function limparCampo() {
     document.getElementById('amigo').value = '';
 }
 
+
 function addAmigos() {
     let amigos = document.getElementById('amigo').value;
 
     if (amigos == '') {
         alert('Por favor, Digite um nome valido!');
+        return
+    }
+    if(verificaCaracteresEspeciais(amigos)) {
+        alert("por favor digite um nome valido!")
+        return
     } else {
         listaDeAmigos.push(amigos);
         let addAmigos = document.getElementById('listaAmigos');
@@ -21,16 +27,15 @@ function addAmigos() {
 function sortearAmigos() {
     if (listaDeAmigos.length === 0) {
         alert('A lista de amigos está vazia')
+    } else if (listaDeAmigos.length != 3){
+        alert('a lista deve conter mais um nome')
         return
     }
     let numeroAleatorio = Math.floor(Math.random() * listaDeAmigos.length)
     let amigoSorteado = listaDeAmigos[numeroAleatorio];
-    //console.log(`o amigo sorteado é: ${amigoSorteado}`)
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `O amigo sorteado é ${amigoSorteado}`;
-
 }
-
 
 const inputAmigo = document.getElementById('amigo');
 const buttonAdd = document.querySelector('.button-add');
@@ -45,3 +50,8 @@ function estadoDoBotao() {
 }
 
 inputAmigo.addEventListener('input', estadoDoBotao);
+
+function verificaCaracteresEspeciais(str){
+    const regex = /[^a-zA-Z0-9 ]/;
+    return regex.test(str)
+}

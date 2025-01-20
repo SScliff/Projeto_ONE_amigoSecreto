99f15419fdddc21e021c44567641aaf40499e8bc
SScliff <sdanielsousa.024@gmail.com>
2025-01-19 22:59:41 -0300

    Adição do confeti ao sortear alguem

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
 const listaDeAmigos = [];
+const inputAmigo = document.getElementById('amigo');
+const buttonAdd = document.querySelector('.button-add');
+
+
 
 function limparCampo() {
     document.getElementById('amigo').value = '';
 }
-
 
 function addAmigos() {
     let amigos = document.getElementById('amigo').value;
 
     if (amigos == '') {
         alert('Por favor, Digite um nome valido!');
-        return
+        return;
     }
-    if(verificaCaracteresEspeciais(amigos)) {
-        alert("por favor digite um nome valido!")
-        return
+
+    if (verificaCaracteresEspeciais(amigos)) {
+        alert("por favor digite um nome sem caracteres especiais!");
+        return;
+
     } else {
         listaDeAmigos.push(amigos);
         let addAmigos = document.getElementById('listaAmigos');
         addAmigos.innerHTML = listaDeAmigos.map((amigo) => `<li>${amigo}</li>`).join('');
         limparCampo();
-        estadoDoBotao()
+        estadoDoBotao();
+
     }
 }
 
 function sortearAmigos() {
     if (listaDeAmigos.length === 0) {
-        alert('A lista de amigos está vazia')
-    } else if (listaDeAmigos.length != 3){
-        alert('a lista deve conter mais um nome')
+        alert('A lista de amigos está vazia');
+        return
+    } else if (listaDeAmigos.length != 3) {
+        alert('a lista deve conter mais um nome');
         return
     }
-    let numeroAleatorio = Math.floor(Math.random() * listaDeAmigos.length)
+    let numeroAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
     let amigoSorteado = listaDeAmigos[numeroAleatorio];
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `O amigo sorteado é ${amigoSorteado}`;
-}
+    resultado.innerHTML = `🎉 ${amigoSorteado} 🎉`;
+    listaDeAmigos.splice(numeroAleatorio, 1)
 
-const inputAmigo = document.getElementById('amigo');
-const buttonAdd = document.querySelector('.button-add');
+    lançarConfetti()
+
+}
 
 function estadoDoBotao() {
 
@@ -51,7 +59,16 @@ function estadoDoBotao() {
 
 inputAmigo.addEventListener('input', estadoDoBotao);
 
-function verificaCaracteresEspeciais(str){
-    const regex = /[^a-zA-Z0-9 ]/;
+function verificaCaracteresEspeciais(str) {
+    const regex = /[^a-zA-Z ]/;
     return regex.test(str)
 }
+
+function lançarConfetti() {
+    confetti({
+        particleCount: 500, // Quantidade de confetes
+        spread: 160,        // Ângulo de dispersão
+        origin: { x: 0.5, y: 0.5 } // Centro da tela
+    });
+}
+
